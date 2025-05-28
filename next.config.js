dad68e13e1c716b6fe11fddbb905d0cd99943109
Mycheze <benadams336@gmail.com>
2025-05-28 12:36:54 +0200

    Fix build issues: update ESLint config and remove project_knowledge folder

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   serverExternalPackages: ['better-sqlite3'],
   webpack: (config) => {
     config.externals.push('better-sqlite3');
+    
+    // Ignore project_knowledge directory
+    config.plugins.push(
+      new (require('webpack')).IgnorePlugin({
+        resourceRegExp: /^\.\/project_knowledge/
+      })
+    );
+    
     return config;
   }
 }
